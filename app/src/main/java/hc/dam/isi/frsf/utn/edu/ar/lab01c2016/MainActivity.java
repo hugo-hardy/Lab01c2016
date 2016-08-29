84aeeef87081ed9c0d2abd62f08b536f4626bc55
Hugo Hardy <huguihardy@gmail.com>
2016-08-28 22:36:02 -0300

    Initial commit

---
+++ app/src/main/java/hc/dam/isi/frsf/utn/edu/ar/lab01c2016/MainActivity.java
@@ -0,0 +1,21 @@
+package hc.dam.isi.frsf.utn.edu.ar.lab01c2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void calcularInteres(){
+    //Calcula el interes para mostrar
+    }
+
+    private void obtenerTasa(){
+    //Obtiene la tasa del archivo xml
+    }
+}
